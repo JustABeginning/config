cd1101eb8a0ef406b53c9101451955b7413bc344
JustABeginning <86421794+JustABeginning@users.noreply.github.com>
2024-08-31 22:31:47 +0530

    config update

--- profile/termux/nvim/lua/snippets/tex.lua
+++ profile/termux/nvim/lua/snippets/tex.lua
@@ -7,56 +7,113 @@ local rep = require("luasnip.extras").rep
 local fmt = require("luasnip.extras.fmt").fmt
 
 -- Snippet Configuration
-ls.add_snippets("tex",{
+ls.add_snippets("tex", {
     -- Environments
     -- \begin{} / \end{}
-    s("beg",fmt(
+    s("beg", fmt(
         [[
         \begin{{{}}}
             {}
         \end{{{}}}
-        ]],{
+        ]], {
             i(1), i(0), rep(1)
         }
     )),
     -- \chapter{}
-    s("chap",fmt(
+    s("chap", fmt(
         [[
         \chapter{{{}}}
-        ]],{
+        ]], {
             i(1)
         }
     )),
     -- \section{}
-    s("sec",fmt(
+    s("sec", fmt(
         [[
         \section{{{}}}
-        ]],{
+        ]], {
             i(1)
         }
     )),
     -- \subsection{}
-    s("subs",fmt(
+    s("subs", fmt(
         [[
         \subsection{{{}}}
-        ]],{
+        ]], {
+            i(1)
+        }
+    )),
+    -- Figure
+    s("figr", fmt(
+        [[
+        \begin{{figure}}[H]
+            {}
+        \end{{figure}}
+        ]], {
+            i(1)
+        }
+    )),
+    -- Table
+    s("tabl", fmt(
+        [[
+        \begin{{table}}[H]
+            {}
+        \end{{table}}
+        ]], {
+            i(1)
+        }
+    )),
+    -- tabularx
+    s("tablx", fmt(
+        [[
+        \begin{{tabularx}}{{{}}}{{{}}}
+            {}
+        \end{{tabularx}}
+        ]], {
+            i(1), i(2), i(0)
+        }
+    )),
+    -- minipage
+    s("minp", fmt(
+        [[
+        \begin{{minipage}}[{}]{{{}}}
+            {}
+        \end{{minipage}}
+        ]], {
+            i(1), i(2), i(0)
+        }
+    )),
+    -- Text
+    -- Italics Text
+    s("it", fmt(
+        [[
+        \textit{{{}}}
+        ]], {
+            i(1)
+        }
+    )),
+    -- Bold Text
+    s("bf", fmt(
+        [[
+        \textbf{{{}}}
+        ]], {
             i(1)
         }
     )),
     -- Math
     -- inline math
-    s("im",fmt(
+    s("im", fmt(
         [[
         ${}$
-        ]],{
+        ]], {
             i(1)
         }
     )),
     -- display math
-    s("dm",fmt(
+    s("dm", fmt(
         [[
         \[{}\]
-        ]],{
+        ]], {
             i(1)
         }
     ))
